e7985bf2f077b9f04b9bb96f01d549e62355b197
Kiran Waghmare <72081819+Kiranwaghmare123@users.noreply.github.com>
2025-12-09 11:36:58 +0530

    Add files via upload

---
+++ Graph/DFS/DFSRecursive/Vertex.java
@@ -0,0 +1,18 @@
+
+
+package dfsRecursive;
+
+public class Vertex 
+{
+	String name;
+	int state;
+	
+	Vertex(String name)
+	{
+		this.name = name;
+	}
+	public String toString()
+	{
+	     return name;
+	}
+}
