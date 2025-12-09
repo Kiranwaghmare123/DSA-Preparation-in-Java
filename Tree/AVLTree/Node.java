f15403647a7519c04bffde0c6716a275defd134d
Kiran Waghmare <72081819+Kiranwaghmare123@users.noreply.github.com>
2025-12-09 11:24:40 +0530

    Add files via upload

---
+++ Tree/AVLTree/Node.java
@@ -0,0 +1,19 @@
+
+
+package avltree;
+
+public class Node 
+{
+	Node lchild;
+	int info;
+	Node rchild;
+	int balance;
+	
+	public Node(int i)
+	{
+		info = i;
+		balance = 0;
+		lchild = null;
+		rchild = null;
+	}
+}
