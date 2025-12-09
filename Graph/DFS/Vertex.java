82d869b1d1b530a85050321ff82c1e22d935cdc1
Kiran Waghmare <72081819+Kiranwaghmare123@users.noreply.github.com>
2025-12-09 11:33:10 +0530

    Add files via upload

---
+++ Graph/DFS/Vertex.java
@@ -0,0 +1,17 @@
+
+package dfs;
+
+public class Vertex 
+{
+	String name;
+	int state;
+	
+	Vertex(String name)
+	{
+		this.name = name;
+	}
+	public String toString()
+	{
+	     return name;
+	}
+}
