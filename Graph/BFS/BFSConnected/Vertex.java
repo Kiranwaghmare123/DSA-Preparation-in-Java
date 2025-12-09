84f5d7992675e1e967b1aafec28313d091ce4fb7
Kiran Waghmare <72081819+Kiranwaghmare123@users.noreply.github.com>
2025-12-09 11:40:07 +0530

    Add files via upload

---
+++ Graph/BFS/BFSConnected/Vertex.java
@@ -0,0 +1,20 @@
+
+
+
+package bfsConnected;
+
+public class Vertex 
+{
+	String name;
+	int state;
+	int componentNumber;
+	
+	Vertex(String name)
+	{
+		this.name = name;
+	}
+	public String toString()
+	{
+	     return name;
+	}
+}
