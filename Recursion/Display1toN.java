baa22f4f511dfb559b0d8f8c2b410c7d0ab9797d
Kiran Waghmare <72081819+Kiranwaghmare123@users.noreply.github.com>
2025-12-09 11:05:51 +0530

    Add files via upload

---
+++ Recursion/Display1toN.java
@@ -0,0 +1,39 @@
+
+
+import java.util.Scanner;
+
+
+public class Display1toN 
+{
+	public static void print1(int n)
+	{
+		if(n==0)
+		   return;
+		System.out.print(n + " ");	
+		print1(n-1);
+	}
+
+	public static void print2(int n)
+	{
+		if(n==0)
+		   return;
+		print2(n-1);
+		System.out.print(n + " ");	
+	}
+
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		int n;
+		System.out.println("Enter value of n : ");
+		n = scan.nextInt();
+		
+		print1(n);	
+		System.out.println();
+		
+		print2(n);	
+		System.out.println();
+		
+		scan.close();
+	}
+}
